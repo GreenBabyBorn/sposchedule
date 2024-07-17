6777098f424bdba66c8a5f588ab0b4bfee6ad0c6
GreenBabyBorn <greenbabyborn@gmail.com>
2024-07-17 22:10:48 +0300

    add: validation for Teacher

--- app/Http/Requests/Teacher/UpdateTeacherRequest.php
+++ app/Http/Requests/Teacher/UpdateTeacherRequest.php
@@ -15,14 +15,36 @@ class UpdateTeacherRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    * Get the validation rules that apply to the request.
+    *
+    * @return array
+    */
+    public function rules()
     {
         return [
-            //
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'patronymic' => 'required|string|max:255',
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'first_name.required' => 'Поле "Имя" обязательно для заполнения.',
+            'first_name.string' => 'Поле "Имя" должно быть строкой.',
+            'first_name.max' => 'Поле "Имя" не должно превышать 255 символов.',
+            'last_name.required' => 'Поле "Фамилия" обязательно для заполнения.',
+            'last_name.string' => 'Поле "Фамилия" должно быть строкой.',
+            'last_name.max' => 'Поле "Фамилия" не должно превышать 255 символов.',
+            'patronymic.required' => 'Поле "Отчество" обязательно для заполнения.',
+            'patronymic.string' => 'Поле "Отчество" должно быть строкой.',
+            'patronymic.max' => 'Поле "Отчество" не должно превышать 255 символов.',
         ];
     }
 }
