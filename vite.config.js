543b6c921d7515d061df5a2069c32e1ae114b5fb
GreenBabyBorn <greenbabyborn@gmail.com>
2024-08-30 12:48:11 +0300

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from "@vitejs/plugin-vue";
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        laravel({
-            input: ['resources/js/app.ts'],
-            refresh: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: "vue/dist/vue.esm-bundler.js",
-        },
+  plugins: [
+    vue(),
+    laravel({
+      input: ['resources/js/main.ts'],
+      refresh: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
     },
+  },
 });
