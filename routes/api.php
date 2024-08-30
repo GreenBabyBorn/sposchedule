6630e63bce19ee89fa3e11b2f344c11d0d509be9
GreenBabyBorn <greenbabyborn@gmail.com>
2024-08-30 14:30:26 +0300

    fix: api.php

--- routes/api.php
+++ routes/api.php
@@ -15,31 +15,67 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/schedules/public', [ScheduleController::class, 'getPublicSchedules']);
 
 // Маршруты, защищенные Sanctum
+Route::get('/groups/courses', [GroupController::class, 'getCourses']);
+Route::get('/groups/{group}/semester/{semester}/schedules/main', [GroupController::class, 'scheduleMain']);
+Route::get('/schedules/changes', [ScheduleController::class, 'getScheduleByDate']);
+Route::apiResource('groups', GroupController::class)->only(['index', 'show']);
+Route::apiResource('lessons', LessonController::class)->only(['index', 'show']);
+Route::apiResource('schedules', ScheduleController::class)->only(['index', 'show']);
+Route::apiResource('subjects', SubjectController::class)->only(['index', 'show']);
+Route::apiResource('teachers', TeacherController::class)->only(['index', 'show']);
+Route::apiResource('semesters', SemesterController::class)->only(['index', 'show']);
+
+// Маршруты, требующие аутентификации
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::get('/groups/courses', [GroupController::class, 'getCourses']);
-    Route::get('/groups/{group}/semester/{semester}/schedules/main', [GroupController::class, 'scheduleMain']);
     Route::post('/groups/{group}/semesters', [GroupController::class, 'attachSemester'])->where(['semester' => '[0-9]+']);
     Route::delete('/groups/{group}/semesters', [GroupController::class, 'detachSemester'])->where(['semester' => '[0-9]+']);
-    Route::apiResource('groups', GroupController::class)->where(['group' => '[0-9]+']);
+    Route::apiResource('groups', GroupController::class)->except(['index', 'show']);
 
     Route::post('/lessons/{lesson}/teachers', [LessonController::class, 'attachTeacher'])->where(['lesson' => '[0-9]+']);
     Route::delete('/lessons/{lesson}/teachers', [LessonController::class, 'detachTeacher'])->where(['lesson' => '[0-9]+']);
-    Route::apiResource('lessons', LessonController::class)->where(['lesson' => '[0-9]+']);
+    Route::apiResource('lessons', LessonController::class)->except(['index', 'show']);
 
-    Route::get('/schedules/changes', [ScheduleController::class, 'getScheduleByDate']);
     Route::patch('/schedules/{schedule}/changes', [ScheduleController::class, 'fromMainToChangesSchedule']);
-    Route::apiResource('schedules', ScheduleController::class);
+    Route::apiResource('schedules', ScheduleController::class)->except(['index', 'show']);
 
-    Route::apiResource('subjects', SubjectController::class)->where(['subject' => '[0-9]+']);
+    Route::apiResource('subjects', SubjectController::class)->except(['index', 'show']);
 
     Route::post('/teachers/{teacher}/subjects', [TeacherController::class, 'attachSubject'])->where(['teacher' => '[0-9]+']);
     Route::delete('/teachers/{teacher}/subjects', [TeacherController::class, 'detachSubject'])->where(['teacher' => '[0-9]+']);
-    Route::apiResource('teachers', TeacherController::class)->where(['teacher' => '[0-9]+']);
+    Route::apiResource('teachers', TeacherController::class)->except(['index', 'show']);
 
-    Route::apiResource('semesters', SemesterController::class)->where(['semester' => '[0-9]+']);
+    Route::apiResource('semesters', SemesterController::class)->except(['index', 'show']);
 });
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::get('/user', function (Request $request) {
+//         return $request->user();
+//     });
+//     Route::post('/logout', [AuthController::class, 'logout']);
+
+//     Route::get('/groups/courses', [GroupController::class, 'getCourses']);
+//     Route::get('/groups/{group}/semester/{semester}/schedules/main', [GroupController::class, 'scheduleMain']);
+//     Route::post('/groups/{group}/semesters', [GroupController::class, 'attachSemester'])->where(['semester' => '[0-9]+']);
+//     Route::delete('/groups/{group}/semesters', [GroupController::class, 'detachSemester'])->where(['semester' => '[0-9]+']);
+//     Route::apiResource('groups', GroupController::class)->where(['group' => '[0-9]+']);
+
+//     Route::post('/lessons/{lesson}/teachers', [LessonController::class, 'attachTeacher'])->where(['lesson' => '[0-9]+']);
+//     Route::delete('/lessons/{lesson}/teachers', [LessonController::class, 'detachTeacher'])->where(['lesson' => '[0-9]+']);
+//     Route::apiResource('lessons', LessonController::class)->where(['lesson' => '[0-9]+']);
+
+//     Route::get('/schedules/changes', [ScheduleController::class, 'getScheduleByDate']);
+//     Route::patch('/schedules/{schedule}/changes', [ScheduleController::class, 'fromMainToChangesSchedule']);
+//     Route::apiResource('schedules', ScheduleController::class);
+
+//     Route::apiResource('subjects', SubjectController::class)->where(['subject' => '[0-9]+']);
+
+//     Route::post('/teachers/{teacher}/subjects', [TeacherController::class, 'attachSubject'])->where(['teacher' => '[0-9]+']);
+//     Route::delete('/teachers/{teacher}/subjects', [TeacherController::class, 'detachSubject'])->where(['teacher' => '[0-9]+']);
+//     Route::apiResource('teachers', TeacherController::class)->where(['teacher' => '[0-9]+']);
+
+//     Route::apiResource('semesters', SemesterController::class)->where(['semester' => '[0-9]+']);
+// });
