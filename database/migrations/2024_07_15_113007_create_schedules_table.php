91ef9a91861c0e3facf38dcddf0b28e3c833238f
GreenBabyBorn <greenbabyborn@gmail.com>
2024-09-16 12:29:25 +0300

    remove: column view_mode

--- database/migrations/2024_07_15_113007_create_schedules_table.php
+++ database/migrations/2024_07_15_113007_create_schedules_table.php
@@ -18,7 +18,7 @@ return new class () extends Migration {
             $table->dateTimeTz('date')->nullable();
             $table->enum('type', ['main', 'changes']);
             $table->enum('week_day', ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'])->nullable();
-            $table->enum('view_mode', ['table', 'message'])->default('table');
+            // $table->enum('view_mode', ['table', 'message'])->default('table');
             // $table->text('message')->nullable();
             $table->boolean('published')->default(false)->nullable();
             $table->timestamps();
