2bc84931e768aa4b8e3e71c217106e79185a3344
GreenBabyBorn <greenbabyborn@gmail.com>
2024-09-18 12:48:06 +0300

    fix: filters Home

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -108,6 +108,11 @@ watch(changesSchedules, (newData) => {
 
 watch([isoDate, selectedCourse, selectedGroup, building], updateQueryParams, { deep: true });
 
+watch(building, () => {
+    course.value = null
+    selectedGroup.value = null
+})
+
 onMounted(() => {
     const dateRegex = /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(\d{4})$/;
 
