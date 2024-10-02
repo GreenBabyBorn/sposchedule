3d8c32983635e6e82f3bd380fbcd4844988c2b8e
GreenBabyBorn <greenbabyborn@gmail.com>
2024-10-02 11:22:15 +0300

    add: yandex metrika

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,10 +11,39 @@
     <title>Пары РКЭ</title>
     <meta name="description" content="Пары РКЭ - оффициальное расписание занятий Рязанского Колледжа Электроники">
     @vite(['resources/js/main.ts'])
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(98521656, "init", {
+            clickmap: true
+            , trackLinks: true
+            , accurateTrackBounce: true
+        });
+
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/98521656" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 
 <body>
     <div id="app"></div>
+
 </body>
 
 </html>
