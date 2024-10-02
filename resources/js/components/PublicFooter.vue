7150d4978f6160f5485c399cc260f2543c3b87b5
GreenBabyBorn <greenbabyborn@gmail.com>
2024-10-02 21:42:15 +0300

    refactor: footer

--- resources/js/components/PublicFooter.vue
+++ resources/js/components/PublicFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="">
         <div class="max-w-screen-xl mx-auto px-4 py-4 flex justify-center">
-            <a href="https://greenbabyborn.ru" target="_blank" class="flex gap-1 items-center text-xs opacity-50">
+            <a href="https://greenbabyborn.ru" target="_blank" class="flex gap-1 items-center text-sm opacity-50">
                 <!-- <span>Сделано с</span>
                 <div class="w-3 h-3">
                     <img class="w-full h-full" src="../../img/emoji.png" alt="любовью">
