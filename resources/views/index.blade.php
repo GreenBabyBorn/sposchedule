442c8b4dd75b68525a887e6647906bb5f7c148f2
GreenBabyBorn <greenbabyborn@gmail.com>
2024-10-02 14:56:58 +0300

    fix: favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="author" content="greenbabyborn">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" type="image/png" href="favicon.png" />
+    <link rel="shortcut icon" type="image/png" href="/favicon.png" />
     <title>Пары РКЭ</title>
     <meta name="description" content="Пары РКЭ - оффициальное расписание занятий Рязанского Колледжа Электроники">
     @vite(['resources/js/main.ts'])
