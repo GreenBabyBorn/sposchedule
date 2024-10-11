261329c3970af3ba226bba556a3e9c281de01a28
GreenBabyBorn <greenbabyborn@gmail.com>
2024-10-11 17:09:42 +0300

    fix: trigger for updated_at on schedule

--- database/migrations/2024_07_15_113021_create_teachers_table.php
+++ database/migrations/2024_07_15_113021_create_teachers_table.php
@@ -13,9 +13,6 @@ return new class () extends Migration {
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->string('first_name');
-            // $table->string('last_name');
-            // $table->string('patronymic');
             $table->timestamps();
         });
     }
--- database/migrations/2024_10_11_140446_add_trigger_to_update_schedule_on_lesson_change.php
+++ database/migrations/2024_10_11_140446_add_trigger_to_update_schedule_on_lesson_change.php
@@ -1,53 +1,70 @@
-Б<?php
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
-class AddTriggerToUpdateScheduleOnLessonChange extends Migration
-{
+return new class () extends Migration {
     public function up()
     {
-        // Создаем функцию для обновления поля updated_at у schedule
+        // Создаем функцию для обновления поля updated_at у schedules
         DB::unprepared('
-CREATE OR REPLACE FUNCTION update_schedule_updated_at()
-RETURNS TRIGGER AS $$
-BEGIN
--- Обновляем расписание по schedule_id, если оно есть
-IF TG_OP = \'INSERT\' OR TG_OP = \'UPDATE\' THEN
-IF NEW.schedule_id IS NOT NULL THEN
-UPDATE schedules
-SET updated_at = NOW()
-WHERE id = NEW.schedule_id;
-END IF;
-ELSIF TG_OP = \'DELETE\' THEN
-IF OLD.schedule_id IS NOT NULL THEN
-UPDATE schedules
-SET updated_at = NOW()
-WHERE id = OLD.schedule_id;
-END IF;
-END IF;
+        CREATE OR REPLACE FUNCTION update_schedule_updated_at()
+        RETURNS TRIGGER AS $$
+        DECLARE
+            current_schedule_id BIGINT;
+        BEGIN
+            -- Проверяем, если операция была на lessons
+            IF TG_TABLE_NAME = \'lessons\' THEN
+                current_schedule_id := NEW.schedule_id; -- Сохраняем schedule_id из NEW записи
+            ELSIF TG_TABLE_NAME = \'lesson_teacher\' THEN
+                -- Получаем schedule_id из lessons, когда срабатывает триггер на lesson_teacher
+                SELECT schedule_id INTO current_schedule_id FROM lessons WHERE id = NEW.lesson_id;
+            END IF;
+        
+            -- Обновляем расписание по schedule_id, если оно есть
+            IF current_schedule_id IS NOT NULL THEN
+                UPDATE schedules
+                SET updated_at = NOW()
+                WHERE id = current_schedule_id;
+            END IF;
+        
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+        ');
 
-RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-');
+        // Удаляем триггер, если он существует
+        DB::unprepared('DROP TRIGGER IF EXISTS update_schedule_on_lesson_change ON lessons');
 
-        // Триггер для таблицы lessons
+        // Создаем триггер для таблицы lessons
         DB::unprepared('
-CREATE TRIGGER update_schedule_on_lesson_change
-AFTER INSERT OR UPDATE OR DELETE ON lessons
-FOR EACH ROW
-EXECUTE FUNCTION update_schedule_updated_at();
-');
+            CREATE TRIGGER update_schedule_on_lesson_change
+            AFTER INSERT OR UPDATE OR DELETE ON lessons
+            FOR EACH ROW
+            EXECUTE FUNCTION update_schedule_updated_at();
+        ');
 
-        // Триггер для таблицы lesson_schedule
+        // Удаляем триггер, если он существует
+        DB::unprepared('DROP TRIGGER IF EXISTS update_schedule_on_lesson_schedule_change ON lesson_schedule');
+
+        // Создаем триггер для таблицы lesson_schedule
         DB::unprepared('
-CREATE TRIGGER update_schedule_on_lesson_schedule_change
-AFTER INSERT OR UPDATE OR DELETE ON lesson_schedule
-FOR EACH ROW
-EXECUTE FUNCTION update_schedule_updated_at();
-');
+            CREATE TRIGGER update_schedule_on_lesson_schedule_change
+            AFTER INSERT OR UPDATE OR DELETE ON lesson_schedule
+            FOR EACH ROW
+            EXECUTE FUNCTION update_schedule_updated_at();
+        ');
+
+        // Удаляем триггер, если он существует
+        DB::unprepared('DROP TRIGGER IF EXISTS update_schedule_on_teacher_change ON lesson_teacher');
+
+        // Создаем триггер для таблицы lesson_teacher
+        DB::unprepared('
+            CREATE TRIGGER update_schedule_on_teacher_change
+            AFTER INSERT OR UPDATE OR DELETE ON lesson_teacher
+            FOR EACH ROW
+            EXECUTE FUNCTION update_schedule_updated_at();
+        ');
     }
 
     public function down()
@@ -55,8 +72,9 @@ EXECUTE FUNCTION update_schedule_updated_at();
         // Удаляем триггеры
         DB::unprepared('DROP TRIGGER IF EXISTS update_schedule_on_lesson_change ON lessons');
         DB::unprepared('DROP TRIGGER IF EXISTS update_schedule_on_lesson_schedule_change ON lesson_schedule');
+        DB::unprepared('DROP TRIGGER IF EXISTS update_schedule_on_teacher_change ON lesson_teacher');
 
         // Удаляем функцию
         DB::unprepared('DROP FUNCTION IF EXISTS update_schedule_updated_at()');
     }
-}
+};
