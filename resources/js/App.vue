41578e1f202f459461871a6b8bec143806b6b721
GreenBabyBorn <greenbabyborn@gmail.com>
2024-10-21 18:46:57 +0300

    remove: bookmark save

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,16 +4,16 @@
   import AppLayout from '@/layouts/AppLayout.vue';
   import { useLoadingStore } from './stores/loading';
   import { useToast } from 'primevue/usetoast';
-  import { onMounted } from 'vue';
+  // import { onMounted } from 'vue';
 
   const toast = useToast();
   const loadingStore = useLoadingStore();
 
-  onMounted(() => {
-    window.addEventListener('load', () => {
-      window.history.replaceState(null, null, window.location.pathname);
-    });
-  });
+  // onMounted(() => {
+  //   window.addEventListener('load', () => {
+  //     window.history.replaceState(null, null, window.location.pathname);
+  //   });
+  // });
 
   axios.interceptors.request.use(config => {
     loadingStore.startLoading(); // Увеличиваем счетчик активных запросов и запускаем загрузку при первом запросе
