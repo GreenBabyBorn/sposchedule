97ad4429c2b840f97f3ffbab597ac803c2e58e94
GreenBabyBorn <greenbabyborn@gmail.com>
2024-10-22 14:21:05 +0300

    add: hourloadview

---
+++ resources/js/pages/admin/HourLoadView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <div class="flex flex-wrap items-baseline justify-between">
+      <h1 class="text-2xl">Нагрузка</h1>
+    </div>
+    <div class="">
+      <form
+        class="flex flex-wrap items-center gap-4 rounded-lg bg-surface-100 p-4 dark:bg-surface-800"
+      ></form>
+    </div>
+  </div>
+</template>
