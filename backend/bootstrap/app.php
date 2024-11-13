beeefd8669a7e632259ca406f67f423d8edf5931
GreenBabyBorn <greenbabyborn@gmail.com>
2024-11-13 23:20:56 +0300

    refactor: Dockerfiles

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         // web: __DIR__.'/../routes/web.php',
         // apiPrefix: 'api',
         commands: __DIR__.'/../routes/console.php',
-        health: '/api/up',
+        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(SecurityHeaders::class);
